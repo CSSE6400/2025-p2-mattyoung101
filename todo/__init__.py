95c7ddbfb5e5a26d50c8f500644a7f4a17f4bd2e
Matt Young <matt.young.1@outlook.com>
2025-03-13 13:46:49 +1000

    Initial stuff

--- todo/__init__.py
+++ todo/__init__.py
@@ -1,9 +1,23 @@
+from todo.views.routes import api
+from todo.models.todo import Todo
+from todo.models import db
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 def create_app():
     app = Flask(__name__)
 
-    from .views.routes import api
-    app.register_blueprint(api)
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite"
 
+    # Load the models.
+    db.init_app(app)
+
+    # Create the database tables.
+    with app.app_context():
+        db.create_all()
+        db.session.commit()
+
+    # Register the blueprints.
+    app.register_blueprint(api)
     return app
